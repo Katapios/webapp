8a4dcefe6e92bb1a386ce5612bf03896f332778b
flamecode <flamel2727@gmail.com>
2017-02-03 16:56:17 +0300

    Test

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,12 @@
  * Created by Игорь on 03.02.2017.
  */
 public class Main {
+    Main(){
+
+    }
+    public String toString(){
+
+    }
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("again");
