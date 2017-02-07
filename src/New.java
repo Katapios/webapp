b3bdf028d5846afd2464cae2be032b1cd89e611f
flamecode <flamel2727@gmail.com>
2017-02-07 18:10:31 +0300

    after lesson01

--- src/New.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by Игорь on 03.02.2017.
- */
-public class New {
-    public static void main(String[] args) {
-
-    }
-}
---
+++ src/by/flame/Resume.java
@@ -0,0 +1,13 @@
+package by.flame;
+
+import java.util.Collection;
+
+/**
+ * Игорь
+ * 07.02.2017.
+ */
+public class Resume {
+    String fullname;
+    String[] some;
+    Collection<String> collection;
+}
