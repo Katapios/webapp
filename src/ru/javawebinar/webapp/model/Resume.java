6ba14e4856bdfa17b74596c682fcbb3172177953
katapios <fresh_idea@inbox.ru>
2018-02-17 13:47:49 +0300

    32

---
+++ src/ru/javawebinar/webapp/model/Resume.java
@@ -0,0 +1,14 @@
+package ru.javawebinar.webapp.model;
+
+import java.util.Collection;
+
+/**
+ * Package: ru.javawebinar.webapp.model
+ * Creator: lordrumin
+ * Has Created: 16.02.2018
+ **/
+public class Resume {
+    String fullName;
+    String[] some;
+    Collection<String> collection;
+}
